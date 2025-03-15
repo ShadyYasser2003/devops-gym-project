f3faf79e9400954f6f43f030041c0074a7381a0a
shady <shady@gmail.com>
2025-03-16 00:18:28 +0200

    1

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-import exampleFunction from '/home/shady_yasser/jenkins/solar-system/sonar-node/src/index.js'; 
-import assert from 'assert'; // استيراد assert من Node.js
-import { expect } from 'chai'; // استيراد expect من chai
+import exampleFunction from './src/index.js'; 
+import assert from 'assert'; 
+import { expect } from 'chai'; 
 
 describe('Basic Tests', function () {
     it('should return true', function () {
-        assert.strictEqual(true, true); // استخدام assert بشكل صحيح
+        assert.strictEqual(true, true);
     });
 });
